504cea06a19a1892bf723587193588b0cdcb6012
Thijs van <80729860+MathijsvanDriel@users.noreply.github.com>
2021-08-05 23:20:22 +0000

    Added Score variable

--- main.py
+++ main.py
@@ -31,6 +31,7 @@ def level (question):
 
 # Sequence 2 
 # Purpose: This sequence asks the user for a name and stores it as a variable
+score=0
 print("Hi there!") 
 time.sleep(1) 
 print("What is your fantastic name?") 
@@ -54,6 +55,8 @@ if played_before == "1":
  if red.lower() == 'whero': 
    print("thats correct " + name)
    print("Lets see what other colours you know!") 
+   score+=1 
+   print("Your score is now" , score)
  else: 
    print("hmm.. you didnt seem to get that one quite right, the correct answer is 'whero'") 
    time.sleep(.5) 
@@ -65,6 +68,8 @@ if played_before == "1":
    if yellow.lower() == 'kowhai': 
     print("Wow! You know your stuff!") 
     print("Lets keep going") 
+    score+=1 
+    print("Your score is now" , score)
    else: 
     print("The correct answer is Kowhai ") 
    
@@ -73,6 +78,8 @@ if played_before == "1":
    green = input() 
    if green.lower() == 'kakariki': 
     print("Correct") 
+    score+=1 
+    print("Your score is now" , score)
    else: 
     print("Green is Kakariki in Te Reo Maaori")  
 
@@ -80,6 +87,8 @@ if played_before == "1":
    orange = input() 
    if orange.lower() == 'karaka': 
     print("Correct") 
+    score+=1 
+    print("Your score is now" , score)
    else: 
     print("The colour orange in te reo Maori is know as karaka") 
   
@@ -89,14 +98,58 @@ elif played_before == "2":
   print("You have selected the intermediate difficulty for this subject!")  
   print("Which word has the meaning of 'Children'") 
 
-  Children = input("\nA. Tama \nB. Poro \nC. Poroiki\nD.Tamariki" + "\n")
+  Children = input("\nA. Tama \nB. Poro \nC. Poroiki\nD.Tamariki\n")
 
   if Children.lower =='D': 
-    print("Correct, well done " + name)
-
+    print("Correct, well done " + name) 
+    score+=1 
+    print("Your score is now" , score)
   else: 
-    print("Incorrect the correct answer was D. Tamariki")") 
+    print("Incorrect, the correct answer was D. Tamariki?")
 
+print("What is the maaori word for 'car'") 
+
+  car = input("\nA. Motuka \nB. Ruaturu \nC. Kahina \nD.Onorere\n")
+
+  if car.lower =='A': 
+    print("Correct, well done " + name) 
+    score+=1 
+    print("Your score is now" , score)
+  else: 
+    print("Incorrect, the correct answer was A. Motuka")
+
+print("What is the maaori word for 'New Zealand?'") 
+
+  Aotearoa = input("\nA. Motu \nB. Aotearoa \nC. Nenuna \nD. Anaterura\n")
+
+  if Aotearoa.lower =='B': 
+    print("Correct, well done " + name) 
+    score+=1 
+    print("Your score is now" , score)
+  else: 
+    print("Incorrect, the correct answer was B. Aotearoa")")  
+
+print("What is the maaori word for 'Man?'") 
+
+  man = input("\nA. Waahine \nB. Werata \nC. Tapu \nD. Tangata\n")
+
+  if man.lower =='C': 
+    print("Correct, well done " + name) 
+    score+=1 
+    print("Your score is now" , score)
+  else: 
+    print("Incorrect, the correct answer was C. Tapu ") 
+
+print("What is the maaori word for 'Time?'") 
+
+  Wa = input("\nA. Wā \nB. Mā \nC. Tā \nD. Ngā\n")
+
+  if wa.lower =='A': 
+    print("Correct, well done " + name) 
+    score+=1 
+    print("Your score is now" , score)
+  else: 
+    print("Incorrect, the correct answer was A. Wā ")
 
 
 elif played_before == "3": 
@@ -109,14 +162,18 @@ elif played_before == "3":
     print("Correct, well done " + name)
 
   else: 
-    print("The correct answer was B. O ngā iwi mātou rā" )
+    print("The correct answer was B. O ngā iwi mātou rā" ) 
+    score+=1 
+    print("Your score is now" , score)
 
   print("If it's Ua it is what?") 
 
   Ua = input("\nA.Foggy \nB.Sunny\nC.Rainy\nD.Thunderstorm" + "\n")
 
   if Ua.lower =='C' or "Rainy": 
-    print("Correct, well done " + name)
+    print("Correct, well done " + name) 
+    score+=1 
+    print("Your score is now" , score)
 
   else: 
     print("The correct answer was C. Rainy" )  
@@ -128,7 +185,9 @@ elif played_before == "3":
 
   if hikoi.lower =='B': 
     print("Correct, well done " + name)
-
+    score+=1 
+    print("Your score is now" , score)
+ 
   else: 
     print("The correct answer was B. Going for a walk")  
 
@@ -137,7 +196,9 @@ elif played_before == "3":
   door = input("\nA.Horoia ō ringaringa \nB.Kuhu mai ki roto\nC.Haere atu\nD.Huakina te kūaha" + "\n")
 
   if door.lower =='D': 
-    print("Correct, well done " + name)
+    print("Correct, well done " + name) 
+    score+=1 
+    print("Your score is now" , score)
 
   else: 
     print("Incorrect; the correct answer was D. Huakina te kūaha")
